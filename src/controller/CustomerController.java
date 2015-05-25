ca6ed9a6fe1c210602b0f9fe9ed2e62b190ce239
taminhtien <taminhtien1993@gmail.com>
2015-05-26 02:04:37 +0700

    Initialize project

---
+++ src/controller/CustomerController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class CustomerController extends ActionSupport{
+
+	@Override
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+	
+}
