eecb621584fb546d24042d5b57909e34b0780ece
taminhtien <taminhtien1993@gmail.com>
2015-05-31 16:41:12 +0700

    Add form for entering card number, r, fix misunderstanding of pin number and card number

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,2 +1,5 @@
-<label><h4>Enter your PIN and press "Enter"</h4></label>
-<input type="text" class="form-control" placeholder="Enter PIN" id="pinNumber">
+<form action="enterCard" method="get" name="enterCardForm">
+	<label><h4>Enter your Card Number and press "Enter"</h4></label>
+	<input type="text" class="form-control" placeholder="Enter Card Number" maxlength="10" id="cardNumber" name="cardNumber">
+	<p name="error"></p>
+</form>
--- WebContent/js/atm.js
+++ WebContent/js/atm.js
@@ -1,18 +1,33 @@
-var pinEntering = "";
+var cardEntering = "";
 
-function inputdata(key)
-{
+function inputdata(key) {
+	var n = cardEntering.length;
+	if (n < 10) {
+		var val = key.value;
+		cardEntering = $('#cardNumber').val() + val;
+		$("#cardNumber").val(cardEntering);
+	}
 
-    var val = key.value;
-    pinEntering = $('#pinNumber').val() + val;
-    $("#pinNumber").val(pinEntering);
-   
 }
 
-$(document).ready(function(){
-    $('#key-clear').click(function(){
-    	pinEntering = "";
-    $("#pinNumber").val(pinEntering);
+function isValidCardNumber(cardNumber) {
+    var isValid = false;
+    var regex = /^([0-9]{6})$/;
+    if (regex.test(cardNumber)) {
+        isValid = true;
+    }
+    return isValid;
+}
+
+$(document).ready(function() {
+	$('#key-clear').click(function() {
+		cardEntering = "";
+		$("#cardNumber").val(cardEntering);
+	});
+	
+	$("#key-enter").click(function() {
+        var cardNumberEntered;
+        cardNumberEntered = $('#cardNumber').val();
+        document.forms['enterCardForm'].submit();
+    });
 });
-});
-   
